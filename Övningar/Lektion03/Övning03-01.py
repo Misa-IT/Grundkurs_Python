d6248b68098b4aa046912e7c66dbc0edf7af81dc
Johan Marmén <johan.marmen@misa.se>
2022-01-28 14:26:42 +0100

    Uppdaterade övningar från lektion 2 och lade till övningar till lektion 3.

--- Övningar/Lektion03/Övning03-01.py
+++ Övningar/Lektion03/Övning03-01.py
@@ -1,6 +1,7 @@
-# Kompletera koden så att "Hello World" bara skrivs ut om 20 är mindre än 30.
+# Lägg till parenteser så att min_variabel får värdet 12 och min_andra_variabel får värdet 16.0.
 
-my_variable =
-my_second_variable =
-if
-print("Hello World")
+min_variabel = 2 + 4 * 2
+print("min_variabel har värdet:", min_variabel)
+
+min_andra_variabel = 20 + 40 / 10 + 10
+print("min_andra_variabel har värdet:", min_andra_variabel)
