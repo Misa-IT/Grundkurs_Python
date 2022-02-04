5b085ca66ce552572f501c93a744b2c2c8d1bd12
Johan Marmén <johan.marmen@misa.se>
2022-02-04 17:13:42 +0100

    Lade till exempel för listor.

---
+++ Exempel/Lektion04/Retrieve_from_list_for_paus_orig.py
@@ -0,0 +1,12 @@
+# Hämta det sista elementet ut listan och skriv ut det.
+# Antingen genom att kolla hur lång listan är eller genom att gå baklänges
+
+# WIP
+# WIP
+
+
+list_with_funny_name = ["En", "lista", "kan", "innehålla", "olika", "saker"]
+
+length_of_list = "Här kan ni skriva en längdräknare"
+
+print()
