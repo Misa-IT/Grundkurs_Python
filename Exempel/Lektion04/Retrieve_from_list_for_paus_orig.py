4126a6ad11a1792d6ff5edf4a096e8989f13fb20
Johan Marmén <johan.marmen@misa.se>
2022-02-09 11:56:02 +0100

    Lade till exempel för listor.

--- Exempel/Lektion04/Retrieve_from_list_for_paus_orig.py
+++ Exempel/Lektion04/Retrieve_from_list_for_paus_orig.py
@@ -1,9 +1,6 @@
 # Hämta det sista elementet ut listan och skriv ut det.
 # Antingen genom att kolla hur lång listan är eller genom att gå baklänges
 
-# WIP
-# WIP
-
 
 list_with_funny_name = ["En", "lista", "kan", "innehålla", "olika", "saker"]
 
