3f426211f8621a616cc588287ca93778c92c575c
Johan <johan.marmen@misa.se>
2022-02-10 10:34:30 +0100

    Add notes from lesson04 and exercises for that week.

--- Övningar/Lektion04/Övning04-01.py
+++ Övningar/Lektion04/Övning04-01.py
@@ -1,2 +1,5 @@
-# Nånting nånting listor
+# Vänd på listan med hjälp av en metod.
 
+en_lista = ["Det", "här", "är", "en", "lista."]
+
+print("Här ska det stå ['lista.', 'en', 'är', 'här', 'Det']:", en_lista)
