30d5b46e6f1701448bc5ff1b60088617f0db9c4e
Johan Marmén <johan.marmen@misa.se>
2022-02-18 15:15:00 +0100

    Changes by henrik

---
+++ Övningar/Lektion06/Övning06-03.py
@@ -0,0 +1,9 @@
+def bedöm_badtemperatur(antal_grader_celsius):
+    pass
+
+
+bedömningar = []
+bedömningar.append(bedöm_badtemperatur(25))
+bedömningar.append(bedöm_badtemperatur(10))
+bedömningar.append(bedöm_badtemperatur(50))
+
