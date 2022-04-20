03f4425953120bbe76d370d88af6a22934af7944
Johan <johan.marmen@misa.se>
2022-04-20 15:20:43 +0200

    Updating examples and notes for Lektion03

--- Exempel/Lektion03/how_many_eggs_with_input.py
+++ Exempel/Lektion03/how_many_eggs_with_input.py
@@ -2,8 +2,6 @@
 
 store_sells_loaves_of_bread = input("Säljer butiken limpor? True eller False: ")
 
-# Vi behöver egentligen inte skriva " == True" men jag skrev det här för att göra det
-# lättare att läsa.
 if store_sells_loaves_of_bread == "True":
     egg_boxes_to_buy = 2
 else:
