d958c96500d68b6e56fb12087e4772ef41652b2d
Johan <johan.marmen@misa.se>
2022-05-18 15:02:31 +0200

    Changes from Lektion06 and Lektion07

---
+++ Exempel/Lektion07/Exempel07-01live.py
@@ -0,0 +1,9 @@
+# Exempel på hur man kan importera funktioner från andra moduler
+# och hur man refererar till dem i koden.
+
+import tobeimported
+
+print(__name__)
+
+if __name__ == '__main__':
+    print("Exempel är main")
