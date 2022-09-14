ebbc69d56d55c69f4f07dac34c0dfa6d5f7ebf7e
Johan <johan.marmen@misa.se>
2022-09-14 15:05:20 +0200

    Changes from Lektion03

---
+++ Exempel/Lektion03/how_many_eggs-live.py
@@ -0,0 +1,9 @@
+store_sells_loaves_of_bread = True
+
+
+if store_sells_loaves_of_bread == True:
+    egg_boxes_to_buy = 2
+elif store_sells_loaves_of_bread == False:
+    egg_boxes_to_buy = 1
+
+print("Det blev", egg_boxes_to_buy, "äggkartonger!")
