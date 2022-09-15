d037e0de5ff3fa1a9d3cb1872ac41e050d1d2edc
Johan <johan.marmen@misa.se>
2022-09-15 16:42:06 +0200

    Clarifications from workshop03

--- Exempel/Lektion03/how_many_eggs.py
+++ Exempel/Lektion03/how_many_eggs.py
@@ -1,9 +1,12 @@
-# Hur många äggkartonger ska vi köpa?
+# Program som säger hur många äggkartonger vi ska köpa.
 
+
+# Vi konstaterar att affären säljer limpor.
 store_sells_loaves_of_bread = True
 
-# Vi behöver egentligen inte skriva " == True" men jag skrev det här för att göra det
-# lättare att läsa.
+
+# Vi behöver egentligen inte skriva " == True" men jag skrev det här
+# för att göra det lättare att läsa.
 if store_sells_loaves_of_bread == True:
     egg_boxes_to_buy = 2
 else:
