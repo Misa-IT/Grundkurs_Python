d3495db31da5c9f634aa1e300b18c1997daddd43
Johan <johan.marmen@misa.se>
2022-09-22 09:29:42 +0200

    Changes from Lektion04

---
+++ Exempel/Lektion04/Exempel04-03-live.py
@@ -0,0 +1,13 @@
+# for-loop
+
+my_str_list = ["Från", "den", "här", "listan", "kan", "vi", "hämta", "text!"]
+my_int_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+for element in my_str_list:
+    print(element)
+    
+print("Vi kan fortfarande läsa element itanför loopen:", element)
+
+print()
+for element in my_int_list:
+    print(element)
---
+++ Exempel/Lektion04/Exempel04-05-live.py
@@ -0,0 +1,12 @@
+my_int_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+for element in my_int_list:
+    print(element)
+
+print()
+
+i = 0
+while i < len(my_int_list):
+    element = my_int_list[i]
+    i = i + 1
+    print(element)
