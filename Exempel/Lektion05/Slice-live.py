bab735e25746725c4b032d39397c94270fd11fb9
Johan <johan.marmen@misa.se>
2022-09-28 15:02:43 +0200

    Changes from Lektion05

---
+++ Exempel/Lektion05/Slice-live.py
@@ -0,0 +1,21 @@
+my_fruits_list = ["Päron", "Äpplen", "Apelsiner", "Citroner", "Vindruvor"]
+
+# Skriv ut hela listan
+print(my_fruits_list)
+
+# Skriv ut de tre första elementen
+print(my_fruits_list[0:3])
+print(my_fruits_list[:3])
+
+# Skriv ut de tre elementen i mitten
+print(my_fruits_list[1:4])
+
+# Skriv ut de tre sista elementen
+print(my_fruits_list[2:5])
+print(my_fruits_list[2:])
+print(my_fruits_list[-3:])
+print(my_fruits_list[len(my_fruits_list)-3:])
+
+
+# Slicing fungerar även på strängar
+print("1234567890"[0:5])
