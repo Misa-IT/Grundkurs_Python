ea74d86f8a40b934f154fa523823721707af1bb1
Johan Marmén <johan.marmen@misa.se>
2022-09-29 11:44:26 +0200

    Changes from Workshop05

--- Övningar/Lektion05/Övning05-01.py
+++ Övningar/Lektion05/Övning05-01.py
@@ -1,5 +1,8 @@
 # Vänd på listan med hjälp av en metod.
 
+
+# Det är inte rätt att ändra i deklarationen av listan, nästa rad ska vara
+# oförändrad.
 en_lista = ["Det", "här", "är", "en", "lista."]
 
 print("Här ska det stå ['lista.', 'en', 'är', 'här', 'Det']:", en_lista)
