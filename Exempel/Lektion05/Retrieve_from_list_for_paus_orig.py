a96ec9292a697a77b34c66aa484d97beeebb5686
Johan Marmén <johan.marmen@misa.se>
2022-12-07 11:00:03 +0100

    Preparing for Lektion05

--- Exempel/Lektion05/Retrieve_from_list_for_paus_orig.py
+++ Exempel/Lektion05/Retrieve_from_list_for_paus_orig.py
@@ -1,5 +1,5 @@
 # Hämta det sista elementet ut listan och skriv ut det.
-# Antingen genom att kolla hur lång listan är eller genom att gå baklänges
+# Antingen genom att kolla hur lång listan är eller genom att gå baklänges.
 # Ni behöver inte använda den förberedda print-funktionen, bara det skrivs ut
 # på något sätt.
 
--- Exempel/Lektion05/Slice.py
+++ Exempel/Lektion05/Slice.py
@@ -1,18 +1,27 @@
-my_fruits_list = ["päron", "äpplen", "apelsiner", "citroner", "vindruvor"]
+my_fruits_list = ["Päron", "Äpplen", "Apelsiner", "Citroner", "Vindruvor"]
 
+# Skriv ut hela listan
+print(my_fruits_list)
+
+# Skriv ut de tre första elementen
 print(my_fruits_list[0:3])
 print(my_fruits_list[:3])
 
+# Skriv ut de tre elementen i mitten
 print(my_fruits_list[1:4])
 
+# Skriv ut de tre sista elementen
 print(my_fruits_list[2:5])
 print(my_fruits_list[2:])
 print(my_fruits_list[-3:])
-
-print("1234567890"[0:5])
+print(my_fruits_list[len(my_fruits_list)-3:])
 
 my_details = ["Johan", "Marmén", "32", "Farsta"]
 
 print(my_details)
 print(my_details[:2])
 print(" ".join(my_details[:2]))
+
+
+# Slicing fungerar även på strängar
+print("1234567890"[0:5])
