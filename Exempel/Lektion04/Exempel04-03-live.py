9f5a5c1291e63e539c7881c98012d529d31f2359
MisaITJohan <johan.marmen@misa.se>
2023-02-24 12:13:57 +0100

    Reversing changes from HT22-2 and preparing for Lektion04

--- Exempel/Lektion04/Exempel04-03-live.py
+++
@@ -1,9 +0,0 @@
-my_str_list = ["Från", "den", "här", "listan", "kan", "vi", "hämta", "text!"]
-my_int_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-
-for element in my_str_list:
-    print(element)
-
-print()
-for element in my_int_list:
-    print(element)
