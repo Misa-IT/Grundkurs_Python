8469e8e04f77b38c0655c473d4046aa03c29086b
MisaITJohan <johan.marmen@misa.se>
2023-03-29 16:57:28 +0200

    Changes from Lektion08

---
+++ Exempel/Lektion08/Exempel08-01-live.py
@@ -0,0 +1,36 @@
+# Exempel på hur man definerar en klass och hur man kan både lagra data
+# samt definera funktioner/metoder däri.
+
+# Vi skapar en ny klass genom att använda detta mönster:
+# Det reserverade ordet class
+# Namnet på vår klass. Namnet bör börja med stor bokstav och ha en stor bokstav
+# i början av varje ord.
+# Ett kolon(:) för att visa att nu börjar ett kodblock.
+
+class MyClass:
+
+    # I klassen kan vi lagra variabler.
+    my_int = 42
+    my_str = "HEJ"
+    my_list = [1, 2]
+
+# Om vi vill skapa en INSTANS av klassen skriver vi så här:
+my_instance = MyClass()
+
+# Notera att det finns en lista i vår nyskapade instans/vårt objekt:
+print("Listan i instansen:", my_instance.my_list)
+
+# Vi testar att lägga till något i listan som ligger i instansen/objektet:
+my_instance.my_list.append(3)
+
+# Vi kollar vad som ligger i listan i instansen och vad som ligger i klassen
+print("Listan i instansen:", my_instance.my_list)
+print("Listan i klassen:", MyClass.my_list)
+# Trots att vi bara ändrar i instansen är det ändrat i klassen.
+# Det är för att en variabel som ligger i en klass delas mellan alla instanser
+# av den klassen om man inte skriver över namnet.
+# Detta kallas för att vara en "klassvariabel".
+
+# Vi skapar en instans till för att bekräfta:
+my_second_instance = MyClass()
+print("Listan i andra instansen:", my_second_instance.my_list)
---
+++ Exempel/Lektion08/Exempel08-04-live.py
@@ -0,0 +1,36 @@
+# Ett lite mer avancerat exempel av arv.
+
+class Animal:
+    sound = "Jag låter som alla andra djur."
+    number_of_legs = 4
+
+    def speak(self):
+        print(self.sound)
+
+
+class Dog(Animal):
+    sound = "Woof!"
+
+    def __init__(self, name, legs=4):
+        self.name = name
+        self.number_of_legs = legs
+        
+
+
+class Cat(Animal):
+
+    def __init__(self, name, prickig):
+        self.name = name
+        self.prickig = prickig
+
+
+
+def main():
+    fido = Dog("Fido")
+    pelle = Cat("Pelle")
+    animals = [fido, pelle]
+    for a in animals:
+        a.speak()
+
+if __name__ == "__main__":
+    main()
