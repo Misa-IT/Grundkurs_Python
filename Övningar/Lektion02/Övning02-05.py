20d5671a7c1a06e964bd8d274340fb228a2527ea
MisaITJohan <johan.marmen@misa.se>
2023-04-19 11:24:48 +0200

    Adding additional exercises for Lektion02.

--- Övningar/Lektion02/Övning02-05.py
+++ Övningar/Lektion02/Övning02-05.py
@@ -7,5 +7,5 @@
 my_list = 2, 3, 4
 my_second_list = [5, 6, 7, 8]
 new_list = my_list + my_second_list
-print("Här ska det stå <class 'list'>: ",type(new_list))
-print("Här ska det stå [2, 3, 4, 5, 6, 7, 8]: ", new_list)
+print("Här ska det stå <class 'list'> när programmet körs: ",type(new_list))
+print("Här ska det stå [2, 3, 4, 5, 6, 7, 8] när programmet körs: ", new_list)
