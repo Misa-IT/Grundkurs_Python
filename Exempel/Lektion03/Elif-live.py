e8664097a024b412b828854c84a85a01bd19431a
MisaITJohan <johan.marmen@misa.se>
2023-05-03 15:06:06 +0200

    Changes from lektion03

---
+++ Exempel/Lektion03/Elif-live.py
@@ -0,0 +1,15 @@
+siffra = 5
+
+if siffra == 1:
+    resultat = 1
+elif siffra == 2:
+    resultat = 2
+elif siffra == 3:
+    resultat = 3
+elif siffra == 3:
+    resultat = "Läses detta?"
+else:
+    print("NU KRASCHAR VI!")
+
+
+print(resultat)
