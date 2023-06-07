7ef7d79fa7e92fa8ed70ee7368a2bac05337e556
MisaITJohan <johan.marmen@misa.se>
2023-06-07 16:00:41 +0200

    Changes from Lektion08

---
+++ Exempel/Lektion08/Exempel08-01-live.py
@@ -0,0 +1,23 @@
+# Exempel på hur man definerar en klass och hur man kan dagra data
+# däri, vi hoppar över metoder i detta exempel.
+
+# Vi skapar en klass genom att använda oss av detta mönster:
+# Det reserverade ordet "class"
+# Namnet på vår klass. Namnet bör börja med stor bokstav och ha en stor bokstav
+# i början av varje ord. Bör inte innehålla åäö/ bör vara alphanummeriskt.
+# Ett kolon (:) för att visa att nu börjar ett kodblock.
+# OM vi ska ha ett arv så har man parenteser också, vi går igenom det senare.
+
+
+class MyClass:
+
+    # I klassen kan vi lagra variabler.
+    my_int = 42
+    my_str = "HEJ"
+    my_list = [1, 2]
+
+# Om vi vill skapa en INSTANS av klassen skriver vi så här:
+my_instance = MyClass()
+
+# Notera att det finns en lista i vår nyskapade instans/vårt objekt.
+print("Listan i instansen:", my_instance.my_list)
--- Exempel/Lektion08/Exempel08-03-live.py
+++ Exempel/Lektion08/Exempel08-03-live.py
@@ -1,17 +1,17 @@
 # Exempel på hur man kan definera hur varje objekt som skapas från en klass
-# ska se ut. Även exempel på instansvarabler.
+# ska se ut. Även exempel på instansvariabler och hur man definerar metoder.
 
 class MyClass:
     my_int = 42
-    my_str = "HEJ"
+    my_str = "HEJ!"
     my_list = [1, 2]
 
-# När vi ska skapa en ny instans/ett nytt objekt av en klass så anropas
-# automatiskt metoden __init__.
-# __init__ är en så kallad Konstruktör.
-# Notera att eftersom __init__ är en metod så måste första argumentet vara self.
+    # När vi ska skapa en ny instans/ett nytt objekt av en klass så anropas
+    # automatiskt metoden __init__.
+    # __init__ är en så kallad konstruktor.
+    # Notera att eftersom __init__ är en metod så måste första argumentet vara self.
     def __init__(self):
-        self.my_instance_list = ["Jag ligger i en instans"]
+        self.my_instance_list = ["Jag ligger i en instans."]
 
 my_instance = MyClass()
 my_second_instance = MyClass()
@@ -19,11 +19,14 @@ print("Listan i klassen:", MyClass.my_list)
 print("Listan i första instansen:", my_instance.my_list)
 print("Listan i andra instansen:", my_second_instance.my_list)
 print()
-#print("Det finns ingen instanslista i klassen:", MyClass.my_inner_list)
-print("Instanslistan i första instansen:", my_instance.my_instance_list)
-print("Instanslistan i andra instansen:", my_second_instance.my_instance_list)
+
+# Nästa rad kommer att krascha programmet så efter att vi bekräftat det så
+# kommenterar vi bort den.
+# print("Det finns ingen inre lista i klassen:", MyClass.my_instance_list)
+print("Inre listan i första instansen:", my_instance.my_instance_list)
+print("Inre listan i andra instansen:", my_second_instance.my_instance_list)
 print()
-# Vi ändrar på instanslistan i den första instansen:
-my_instance.my_instance_list.append("Jag ska hamna i den första instansens lista!")
-print("Instanslistan i första instansen:", my_instance.my_instance_list)
-print("Instanslistan i andra instansen:", my_second_instance.my_instance_list)
+
+my_instance.my_instance_list.append("Jag ska hamna i den första instansens lista")
+print("Inre listan i första instansen:", my_instance.my_instance_list)
+print("Inre listan i andra instansen:", my_second_instance.my_instance_list)
