d4ccd3c3ac6a0c3e526a661c219bf4e2ffe45956
MisaITJohan <johan.marmen@misa.se>
2024-05-30 12:56:25 +0200

    Updates from L04

--- Exempel/Lektion04/Exempel04_01_live.py
+++ Exempel/Lektion04/Exempel04_01_live.py
@@ -3,10 +3,10 @@
 my_int = 1
 my_second_int = 10
 
+# Saker ska ske så länge my_int är mindre än my_second_int
 while my_int < my_second_int:
     print(my_int)
     my_int = my_int + 1
     print(my_int)
-
 
 print("Nu är loopen över!")
--- Exempel/Lektion04/Exempel04_03_live.py
+++ Exempel/Lektion04/Exempel04_03_live.py
@@ -3,10 +3,15 @@
 my_str_list = ["Från", "den", "här", "listan", "kan", "vi", "hämta", "text!"]
 my_int_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
+# Skriv ut hela listan
+print(my_str_list)
+
+# Skriv ut varje element
 for element in my_str_list:
     print(element)
 print("Vi kan fortfarande läsa element utanför loopen:", element)
 
-print()    # För att lägga in ett radbyte.
+print() # För att lägga in ett radbyte
 for element in my_int_list:
     print(element)
+print("Vi kan fortfarande läsa element utanför loopen:", element)
