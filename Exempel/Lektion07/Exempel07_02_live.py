f6c963b13912e048efc7f0d7a65546bc584cc5f4
MisaITJohan <johan.marmen@misa.se>
2024-06-27 11:36:52 +0200

    From 2024-VT2-W07

--- Exempel/Lektion07/Exempel07_02_live.py
+++ Exempel/Lektion07/Exempel07_02_live.py
@@ -1,2 +1,18 @@
 # Första gången vi importerar en modul i vårt program laddas
 # den från disk och koden i modulen körs.
+
+print("Importerar modulen tobeimported_live...")
+import tobeimported_live
+print("Importerad modul:", tobeimported_live)
+
+# Observera att koden i tobeimported_live inte körs andra gången,
+# om vi importerar modulen igen eftersom vi har laddat den
+# tidigare.
+print()
+
+input("Vi väntar på att du ska ändra något i tobeimported_live.")
+
+print()
+print("Importerar samma modul igen!")
+import tobeimported_live
+print("Fortfarande samma modul:", tobeimported_live)
--- Exempel/Lektion07/Exempel07_03_live.py
+++ Exempel/Lektion07/Exempel07_03_live.py
@@ -1,3 +1,51 @@
 # Ett exempel på en "praktisk" användning av en inbyggd modul som måste
 # importeras. Även ett exempel på varför det kan vara viktigt att följa
 # stilregler.
+
+import random as ran
+import random
+from random import sample
+
+my_card_list = ["Ruter Två", "Ruter Tre", "Ruter Fyra", "Ruter Fem",
+                "Ruter Sex", "Ruter Sju", "Ruter Åtta", "Ruter Nio",
+                "Ruter Tio", "Ruter Knekt", "Ruter Drottning",
+                "Ruter Kung", "Ruter Äss", "Hjärter Två", "Hjärter Tre",
+                "Hjärter Fyra", "Hjärter Fem", "Hjärter Sex", "Hjärter Sju",
+                "Hjärter Åtta", "Hjärter Nio", "Hjärter Tio", "Hjärter Knekt",
+                "Hjärter Drottning", "Hjärter Kung", "Hjärter Äss",
+                "Klöver Två", "Klöver Tre", "Klöver Fyra", "Klöver Fem",
+                "Klöver Sex", "Klöver Sju", "Klöver Åtta", "Klöver Nio",
+                "Klöver Tio", "Klöver Knekt", "Klöver Drottning",
+                "Klöver Kung", "Klöver Äss", "Spader Två", "Spader Tre",
+                "Spader Fyra", "Spader Fem", "Spader Sex", "Spader Sju",
+                "Spader Åtta", "Spader Nio", "Spader Tio", "Spader Knekt",
+                "Spader Drottning", "Spader Kung", "Spader Äss"]
+
+print("En slumpad dragning av fem spelkort:", sample(my_card_list, k=5))
+
+print()  # Tom print() för att göra en tom rad.
+# Om vi multiplicerar en sträng så upprepas strängen så många gånger.
+print("------------"*5)
+
+# Vi skapar en ny lista och slumpar ordningen:
+# my_random_list = my_card_list  # Detta är fel, men vi kollar vad som händer.
+my_random_list = []
+for card in my_card_list:
+    my_random_list.append(card)
+# my_random_list = [card for card in my_card_list]  # Alternativt sätt.
+
+print("De ursprungliga listorna:\n")
+print("My card list:\n", my_card_list)
+print("------")
+print("My random list:\n", my_random_list)
+
+print("------------"*5)
+x = ran.shuffle(my_random_list)
+print("Värdet på x är:", x)
+
+
+print("-------"*5)
+print("Listorna efter blandning:")
+print("My card list:\n", my_card_list)
+print("------")
+print("My random list:\n", my_random_list)
