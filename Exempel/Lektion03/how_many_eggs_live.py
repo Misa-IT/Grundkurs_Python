82a8cffbb6c99a0f72e54e63ec7869a8d7f1294e
MisaITJohan <johan.marmen@misa.se>
2024-09-17 14:58:48 +0200

    Changes from 2024-HT1-L03

--- Exempel/Lektion03/how_many_eggs_live.py
+++ Exempel/Lektion03/how_many_eggs_live.py
@@ -3,18 +3,17 @@
 
 
 # Vi konstaterar att affären säljer limpor.
-store_sells_loaves_of_bread = True
+store_sells_loaves_of_bread = False
 
 
 # Vi behöver egentligen inte skriva " == True" men jag skriver
-# det här för att göra koden lättare att läsa.
+# det här för att göra koden enklare att läsa för er.
 if store_sells_loaves_of_bread == True:
     egg_boxes_to_buy = 2
 else:
     egg_boxes_to_buy = 1
 
-
 if egg_boxes_to_buy == 2:
-    print("Nu tänkte du fel, det var inte inte så jag menade.")
+    print("Nu tänkte du fel, det var inte så jag menade.")
 
 print("Det blev", egg_boxes_to_buy, "äggkartonger!")
