8fcd7c24cd8eddba59f18f300c531d58f7c04364
MisaITJohan <johan.marmen@misa.se>
2024-09-25 09:55:51 +0200

    Add exercise to W04

---
+++ Övningar/Lektion04/Övning04_05.py
@@ -0,0 +1,7 @@
+# Skriv ett program som ber användaren att skriva in ett namn tills användaren
+# skriver in något särskilt ord, förslagsvis "quit".
+
+# Programmet ska skriva ut en hälsning varje gång användaren skriver in ett namn.
+# Om användaren skriver in t.ex. "Johan" så ska programmet skriva ut något i
+# stil med "Hej Johan! Trevligt att träffas!"
+
