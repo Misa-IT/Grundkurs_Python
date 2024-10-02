a18d0ebe513efbb531a29e41c0cde7b1bfbd039a
MisaITJohan <johan.marmen@misa.se>
2024-10-02 10:58:19 +0200

    Add three new exercises to W05

---
+++ Övningar/Lektion05/Övning05_06.py
@@ -0,0 +1,35 @@
+# Komplettera koden nedan så att det sista elementet i listan hämtas och skrivs
+# ut en gång för varje varv i loopen.
+# Antingen genom att kolla hur lång listan är eller genom att gå baklänges.
+
+# Det som ska skrivas ut är alltså:
+# saker
+# 0
+# 1
+# 2
+# 3
+# 4
+# 5
+# 6
+# 7
+# 8
+
+
+
+
+list_with_funny_name = ["En", "lista", "kan", "innehålla", "olika", "saker"]
+
+
+i = 0
+while i < 10:
+    # Ingenting ska ändras på denna rad eller de ovanför.
+
+
+    print()
+
+
+    # Ingenting ska ändras på denna rad eller de nedanför.
+    list_with_funny_name.append(i)
+    i = i + 1
+
+
---
+++ Övningar/Lektion05/Övning05_07.py
@@ -0,0 +1,22 @@
+# Skriv ett program som gör följande:
+#
+#   - Ber användaren skriva in ett värde tre gånger.
+#   - De tre värdena ska vara:
+#       - Namn
+#       - Ålder
+#       - Längd
+#
+#   - De tre värdena ska lagras i en tuple.
+#       Tänk på att du inte kan ändra på en tuple efter att den är skapad.
+#
+#   - Efter att tupeln är skapad så ska den förberedda print() skriva ut
+#       de tre värdena som ligger i tupeln.
+#
+#       NOTERA! Ni måste alltså ändra i den print() som är förberedd så att
+#           värdena som skrivits in hämtas från tupeln.
+#
+#
+#       FÖRTYDLIGANDE:
+#           Det är alltså strängarna med xxxx som ska bytas ut mot något.
+
+print("xxxxxxxxx", " är ", "xxxxxxx", " år gammal och ", "xxxxxxx", "cm lång.", sep="")
