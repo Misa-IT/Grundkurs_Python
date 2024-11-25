5ee2089143a747d3884231ff5c66d1aaf7d2b630
MisaITJohan <johan.marmen@misa.se>
2024-11-25 16:58:34 +0100

    Apply preparations for 2024-HT2-L03

--- Exempel/Lektion03/how_many_eggs_live.py
+++ Exempel/Lektion03/how_many_eggs_live.py
@@ -1,19 +1,2 @@
 # Program som säger hur många äggkartonger vi ska köpa.
 # Alltså ett exempel på en if-sats.
-
-
-# Vi konstaterar att affären säljer limpor.
-store_sells_loaves_of_bread = False
-
-
-# Vi behöver egentligen inte skriva " == True" men jag skriver
-# det här för att göra koden enklare att läsa för er.
-if store_sells_loaves_of_bread == True:
-    egg_boxes_to_buy = 2
-else:
-    egg_boxes_to_buy = 1
-
-if egg_boxes_to_buy == 2:
-    print("Nu tänkte du fel, det var inte så jag menade.")
-
-print("Det blev", egg_boxes_to_buy, "äggkartonger!")
