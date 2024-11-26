556263e258c44865f2b380ec1705d9798e3be630
MisaITJohan <johan.marmen@misa.se>
2024-11-26 12:14:32 +0100

    Apply changes from 2024-HT2-L03

--- Exempel/Lektion03/Elif_live.py
+++ Exempel/Lektion03/Elif_live.py
@@ -1,15 +1,16 @@
-siffra = 3
+siffra = int(input("Vilken siffra? "))
+resultat = "OGILTIGT"
 
-if siffra == 1:
+if siffra <= 1:
     resultat = 1
     print("Siffra är 1")
-elif siffra == 2:
+elif siffra <= 2:
     resultat = 2
     print("Siffra är 2")
-elif siffra == 3:
+elif siffra <= 3:
     resultat = 3
     print("Siffra är 3")
 else:
-    print("Hittade inget korrekt val.")
+    print("Hittade inget korrekt val!")
 
 print(resultat)
