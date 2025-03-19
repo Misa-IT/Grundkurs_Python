c8a1e00b687ef23fd899615ea15bc85ad44c2ff6
MisaITJohan <johan.marmen@misa.se>
2025-03-19 14:22:32 +0100

    Clarify Övning07_01

--- Övningar/Lektion07/Övning07_01.py
+++ Övningar/Lektion07/Övning07_01.py
@@ -1,5 +1,6 @@
 # Här försöker vi använda funktionen choice från paketet random men
-# något är fel med koden. Rätta till så att det fungerar.
+# något är fel med koden. Rätta till så att den fungerar och vi får ut ett
+# slumpvist valt element.
 
 import random
 
