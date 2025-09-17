cad9e0335f9560633dcfb19653daab520fe8fd90
MisaITJohan <johan.marmen@misa.se>
2025-09-17 11:43:07 +0200

    Update exercise instructions to clarify string concatenation rules

--- Övningar/Lektion02/Övning02_01.py
+++ Övningar/Lektion02/Övning02_01.py
@@ -1,6 +1,9 @@
 # Något i denna kodrad gör att programmet inte fungerar, rätta felet så att vi får resultatet
 # Hello World1234
+
 # Att ändra till print("Hello World1234") är inte rätt svar.
+# Att ändra till print("Hello World" + "1234") är inte rätt svar.
+
 # Använd er av vad som ni fått lära er under lektion02.
 
 print("Hello World" + 1234)
