7802a11fc104320367cca665f53442e00d698010
MisaITJohan <johan.marmen@misa.se>
2025-09-23 17:02:41 +0200

    Apply changes from 2025-HT1-L03

--- Exempel/Lektion03/Elif_live.py
+++ Exempel/Lektion03/Elif_live.py
@@ -1,21 +1,23 @@
-siffra = -1
+# Exempel på if-satser som innehåller elif-klausuler.
 
+siffra = 8
+#resultat = "okänt"
 
-if siffra >= 1:
+if siffra <= 1:
     resultat = 1
     print("Siffra är 1")
-elif siffra >= 2:
+elif siffra <= 2:
     resultat = 2
     print("Siffra är 2")
-elif siffra >= 3:
+elif siffra <= 3:
     resultat = 3
     print("Siffra är 3")
-elif siffra >= 4:
+elif siffra <= 4:
     resultat = 4
-elif siffra >= 5:
+    prntrds
+elif siffra <= 5:
     resultat = 5
 else:
-    resultat = "okänt"
     print("Hittade inget korrekt val!")
 
-print("Resultat är", resultat)
+print("resultat är", resultat)
