4e2220fda37c35f83073a1fcd3b650ed0faeacfb
MisaITJohan <johan.marmen@misa.se>
2025-11-10 16:17:32 +0100

    Apply preparations for 2025-HT2-L02

--- Exempel/Lektion02/Exempel02_02_cast_input_live.py
+++ Exempel/Lektion02/Exempel02_02_cast_input_live.py
@@ -5,7 +5,7 @@ tal = int(input("Vilket tal ska vi dubbla? "))
 print(tal * 2)
 print(type(tal))
 # type() är en så kallad Funktion och vi använder den här för att kontrollera
-# vad för "typ" det som vi har innanför parentserna har.
+# vad för "typ" det som vi har innanför parenteserna har.
 
 tal = input("Vilket tal ska vi dubbla? ")
 
