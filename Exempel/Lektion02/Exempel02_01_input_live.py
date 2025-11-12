e88cada4f409223ea90da919a2b98aa6ad853424
MisaITJohan <johan.marmen@misa.se>
2025-11-12 12:03:06 +0100

    Apply changes from 2025-HT2-L02 and W02

--- Exempel/Lektion02/Exempel02_01_input_live.py
+++ Exempel/Lektion02/Exempel02_01_input_live.py
@@ -1,14 +1,17 @@
 # Exempel av input()
 
-# input() visar en sträng för användaren, en så kallad prompt,
+# imput() visar en sträng för användaren, en så kallad prompt,
 # som bör uppmana användaren att skriva in ett svar.
 # Svaret hämtas som en sträng, alltså som text, och kan sen sparas i en variabel
 # eller användas direkt.
+
 
 print("Hej, vad heter du?")
 
 user_name = input("Mitt namn är: ")
 
-user_name = input("Hej, vad heter du?\nMitt namn är: ")
+
+# user_name = input("Hej, vad heter du?\nMitt namn är: ")
+
 
 print("Trevligt att träffas, " + user_name + ".")
