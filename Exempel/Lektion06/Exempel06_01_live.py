98abed93f6929b04dec464b8f0eb4a78ee0905ca
MisaITJohan <johan.marmen@misa.se>
2025-12-10 11:18:07 +0100

    Apply changes from 2025-HT2-L06 and W06

--- Exempel/Lektion06/Exempel06_01_live.py
+++ Exempel/Lektion06/Exempel06_01_live.py
@@ -8,7 +8,7 @@ def hello_world():
     print("Hello, World!")
 
 
-# Man kan också ge funktioner data som de behöver, i form av
+# Man kan också ge Funktioner data som de behöver, i form av
 # s.k. Argument, som då inte behöver delas i hela programmet.
 #
 # I det här exemplet accepterar vår Funktion ett Argument, ett namn,
@@ -24,12 +24,12 @@ hello_world()
 hello("Johan")
 
 
-# Funktioner kan också ge värden tillbaka, vilket man kallar
-# att RETURNERA ett värde. De liknar på det sättet Funktioner
-# inom matematiken. Det returnerad värdet hamnar därefter på samma plats i
+# Funktioner kan också ge värden tillbaka, vilket man kallar för att
+# RETURNERA ett värde. De liknar på det sättet Funktioner
+# inom matematiken. Det returnerade värdet hamnar därefter på samma plats i
 # koden som funktionsanropet skedde.
 #
-# I det här exemplet returnerar vi argumentet multiplicerat med två.
+# I det här exemplet returnerar vi Argumentet multiplicerat med två.
 # Notera att det returnerade värdet nedan inte skrivs ut om vi inte skickar
 # det vidare till print().
 
@@ -38,10 +38,10 @@ def f(x):
 # Exemplet ovan är t.ex. väldigt likt den matematiska Funktionen f(x)=x*2
 
 
-# Notera skillanden på vad som händer på nedanstående rader.
-f(3)
-print(f(3))
-dubblat = f(4)
+# Notera skillnaden på vad som händer på nedanstående rader.
+f(1)
+print(f(2))
+dubblat = f(3)
 print(dubblat)
-f(8)
-16
+f(4)
+8
--- Exempel/Lektion06/exempelfunktioner_live.py
+++ Exempel/Lektion06/exempelfunktioner_live.py
@@ -1,35 +1,35 @@
 # Exempel på hur man definierar, skapar, egna funktioner:
 
 
-# def för att meddela att vi DEFINERAR vad en ny funktion betyder.
-# Namnet på funktionen. Bör vara lätt att förstå och BÖR inte innehålla åäö. Namnet
-# bör också vara i små bokstäver med understreck(_) istället för mellanslag.
-# Parenteser, (), För att de MÅSTE vara där även om vi inte planerar att använda
-# argument med denna funktion.
-# Ett kolon,(:), för att påbörja själva definitionen.
+# def för att meddela att vi DEFINIERAR vad en ny Funktion betyder.
+# Namnet på Funktionen. Bör vara lätt att förstå och BÖR inte innehålla åäö. Namnet
+#   bör också vara i små bokstäver med understreck(_) istället för mellanslag.
+# Parenteser, (). För att de MÅSTE vara där även om vi inte planerar att använda
+#   argument med denna Funktion.
+# Ett kolon (:) för att påbörja själva definitionen.
 # En indentering på alla rader som tillhör Funktionen.
 
 def exempelfunktion():
-    print("Vår funktion gör inget för stunden så jag lägger in lite text så länge.")
+    print("Vår Funktion gör inget för stunden så jag lägger in lite text så länge.")
 
 
-# När vi vill köra vår funktion, även kallat för att "anropa" eller "kalla" på den, så
-# måste vi använda parenteser, (), efter namnet för att meddela att vi faktiskt vill
-# köra funktionen.
+# När vi vill köra vår Funktion, även kallat för att "anropa" eller "kalla" på den, så
+#   måste vi använda parenteser, (), efter namnet för att meddela att vi faktiskt vill
+#   köra funktionen.
 exempelfunktion()
 
 
 # Om vi vill skapa en Funktion som ska kunna ta emot instruktioner om vad den ska
-# arbeta med så måste vi skriva in det innanför parenteserna när vi
-# DEFINIERAR vår Funktion.
+#   arbeta med så måste vi skriva in det innanför parenteserna när vi
+#   DEFINIERAR vår Funktion.
 # T.ex. vilken text som ska skrivas ut av print() eller vilka tal som ska
-# läggas ihop av en funktion som adderar två tal.
+#   läggas ihop av en Funktion som adderar två tal.
 
 def funktion_som_tar_emot_tre_argument(godtyckligt_namn1, godtyckligt_namn2, godtyckligt_namn3=8):
     print(godtyckligt_namn1, godtyckligt_namn2, godtyckligt_namn3)
 
 # Det som vi skriver inom parenteserna i DEFINITIONEN är alltså vad vi namnger
-# det som skickas till vår funktion som argument varje gång den körs.
+#   det som skickas till vår Funktion som Argument varje gång den körs.
 
 funktion_som_tar_emot_tre_argument(8, "HEJ", 4.567)
 funktion_som_tar_emot_tre_argument(4.567, godtyckligt_namn3=5, godtyckligt_namn2="HEJ")
