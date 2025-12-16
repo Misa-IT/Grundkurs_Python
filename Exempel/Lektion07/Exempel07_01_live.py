1daeeaae4ba2f4001ad5f7f7e611982e3a72b107
MisaITJohan <johan.marmen@misa.se>
2025-12-16 16:38:20 +0100

    Apply changes from 2025-HT2-L07

--- Exempel/Lektion07/Exempel07_01_live.py
+++ Exempel/Lektion07/Exempel07_01_live.py
@@ -1,3 +1,17 @@
 # Exempel på hur man kan importera funktioner och annat från andra
 # moduler och hur man refererar till dem i koden.
 
+import tobeimported_live
+
+tobeimported_live.my_function()
+
+# Variabeln __name__ får olika värden beroende på vilken
+# fil man startade programmet ifrån. Den modulen blir då
+# "the main module", huvudmodulen.
+print("__name__ från Exempel07_01_live:", __name__)
+print("__name__ från  tobeimported_live i Exempel07_01_live:",
+      tobeimported_live.__name__)
+
+print()
+if __name__ == "__main__":
+    print("Exempel07_01_live är main")
--- Exempel/Lektion07/tobeimported_live.py
+++ Exempel/Lektion07/tobeimported_live.py
@@ -1,2 +1,21 @@
 # Här definierar vi saker som vi kan hämta från andra moduler.
 
+def my_function():
+    print("Detta skrivs ut av funktionen i tobeimported_live.py")
+
+
+# För att demonstrera hur importfinessen fungerar visar vi ett meddelande
+# här, vilket man i vanliga fall inte skulle göra i en modul som är
+# tänkt att importeras.
+print("Den här strängen skrivs ut i tobeimported_live")
+print("__name__ ifrån  tobeimported_live:", __name__)
+
+x = input("Vi sätter ett värde på x i tobeimported_live! Skriv in något som ska lagras här: ")
+print("Vi skriver ut x medans vi är i tobeimported_live:", x)
+print()
+
+def main():
+    print("tobeimported_live är main")
+
+if __name__ == "__main__":
+    main()
