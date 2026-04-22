39f67db4a65775c7080ff574ee8ef4f9db2d4412
MisaITJohan <johan.marmen@misa.se>
2026-04-22 11:22:02 +0200

    Apply changes from  2026-VT2-L02

--- Övningar/Lektion02/Övning02_04.py
+++ Övningar/Lektion02/Övning02_04.py
@@ -4,7 +4,7 @@ x = 48
 # Raderna här under får programmet att krascha, rätta till dem så att de gör
 # rätt sak (vi vill få ut tre rader; en med 52, en med 48 och en med 100).
 
-# Att skriva print(52), print(38) och print(100) är inte rätt svar.
+# Att skriva print(52), print(48) och print(100) är inte rätt svar.
 
 print(A)
 print(X)
